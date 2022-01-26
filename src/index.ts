ebed2782a063f9c820dcb11bb574c1c549dcb744
jaywcjlove <398188662@qq.com>
2022-01-26 14:23:19 +0800

    generator images string.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
+import path from 'path';
 import { setFailed, getInput, info } from '@actions/core';
 import { getOctokit, context } from '@actions/github';
+import image2uri from 'image2uri';
 
-// InstanceType<typeof GitHub>
+const svgStr = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="890" height="262">
+<style>.github-contributors-svg { cursor: pointer; }</style>
+{{avatar}}
+</svg>`;
+
+type Data = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  contributions: number;
+}
 
 class Generator {
   token: string;
   owner: string;
   repo: string;
+  output: string;
+  svg: string;
+  data: Array<Data> = [];
   constructor() {
     const { owner, repo } = context.repo;
     if (!repo) {
@@ -15,13 +45,21 @@ class Generator {
     if (!owner) {
       setFailed(`owner name does not exist!`);
     }
+    this.repo = repo;
+    this.owner = owner;
+
     const myToken = getInput('token');
     if (!myToken) {
       setFailed(`token does not exist!`);
     }
     this.token = myToken;
-    this.repo = repo;
-    this.owner = owner;
+
+    const output = getInput('output');
+    if (!output) {
+      setFailed(`'output' does not exist!`);
+    }
+    this.output = path.resolve(process.cwd(), output);
+    info(`output: \x1b[34m${this.output}\x1b[0m`);
     info(`owner/repo: \x1b[34m${owner}/${repo}\x1b[0m`);
   }
   async getContributors() {
@@ -30,15 +68,27 @@ class Generator {
       owner: this.owner,
       repo: this.repo
     });
+    if (list.data && list.data.length > 0) {
+      this.data = list.data;
+    }
     return list
+  }
+  generator() {
+    const avatar = this.data.map((item, idx) => {
+      return `<a xlink:href="https://github.com/${item.login}" class="github-contributors-svg" target="_blank" rel="nofollow sponsored" id="${item.login}">
+<image x="106" y="210" width="24" height="24" xlink:href="${image2uri(item.avatar_url, { ext: '.apng' })}"/>
+</a>`
+    });
+    return svgStr.replace('{{avatar}}', avatar.join(''))
   }
 }
 
 try {
   ;(async () => {
     const gen = new Generator();
-    const list = await gen.getContributors();
-    console.log(list)
+    await gen.getContributors();
+    const str = gen.generator()
+    console.log(str)
   })();
 } catch (error) {
   setFailed(error.message);
