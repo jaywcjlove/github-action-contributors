a6afca593883b9ff5f13b40f3f5296a8a7079934
jaywcjlove <398188662@qq.com>
2022-01-26 17:50:24 +0800

    chore: add lib folder.

---
+++ lib/index.d.ts
@@ -0,0 +1,12 @@
+export declare function getInputs(): {
+    count: number;
+    includeBots: boolean;
+    affiliation: "all" | "direct" | "outside";
+    svgTemplate: string;
+    svgPath: string;
+    truncate: number;
+    svgWidth: number;
+    avatarSize: number;
+    avatarMargin: number;
+    userNameHeight: number;
+};
