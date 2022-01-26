fbf5c9533fe54ca5e4c4a40e82a60de04969251b
jaywcjlove <398188662@qq.com>
2022-01-26 12:56:00 +0800

    init project.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# github-action-contributors
+github-action-contributors
+===
+
 Github action generates dynamic image URL for contributor list to display it!
+
+## Quick Start
+
+```shell
+$ npm install
+
+$ npm run watch # Listen compile .ts files.
+$ npm run build # compile .ts files.
+
+$ npm run start
+```
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import core, { } from '@actions/core';
+import github, { getOctokit } from '@actions/github';
+
+// InstanceType<typeof GitHub>
+
+class Generator {
+  token: string;
+  owner: string;
+  repo: string;
+  constructor() {
+    const { owner, repo } = github.context.repo;
+    if (!repo) {
+      core.setFailed(`repo name does not exist!`);
+    }
+    if (!owner) {
+      core.setFailed(`owner name does not exist!`);
+    }
+    const myToken = core.getInput('token');
+    if (!myToken) {
+      core.setFailed(`token does not exist!`);
+    }
+    this.token = myToken;
+    this.repo = repo;
+    this.owner = owner;
+    core.info(`owner/repo: \x1b[34m${owner}/${repo}\x1b[0m`);
+  }
+  async getContributors() {
+    const octokit = getOctokit(this.token);
+    const list = await octokit.request(`GET /repos/${this.owner}/${this.repo}/contributors`, {
+      owner: this.owner,
+      repo: this.repo
+    });
+    return list
+  }
+}
+
+;(async () => {
+  try {
+    const gen = new Generator();
+    const list = await gen.getContributors();
+    console.log(list)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
